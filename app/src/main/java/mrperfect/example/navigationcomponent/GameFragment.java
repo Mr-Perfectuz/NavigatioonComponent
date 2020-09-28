eadafd2fb73554e0f2acfedef772cac02e318fe0
Mr.Perfect <ziyodovanvar1999@gmail.com>
2020-09-28 07:26:44 +0500

    StartFragment has been activated

--- app/src/main/java/mrperfect/example/navigationcomponent/GameFragment.java
+++ app/src/main/java/mrperfect/example/navigationcomponent/GameFragment.java
@@ -2,13 +2,18 @@
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
+    /**
  * A simple {@link Fragment} subclass.
  * Use the {@link GameFragment#newInstance} factory method to
  * create an instance of this fragment.
@@ -61,4 +66,19 @@ public class GameFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_game, container, false);
     }
+
+        @Override
+        public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+
+            final NavController navController = Navigation.findNavController(view);
+
+            Button button = view.findViewById(R.id.Finish_Game_Button);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    navController.navigate(R.id.action_gameFragment_to_endGameFragment);
+                }
+            });
+        }
 }
